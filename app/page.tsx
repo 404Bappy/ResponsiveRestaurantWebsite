f09dd1942c9605dbe32a9fb08acf31c04a8b07bb
Bappy71 <19103076@iubat.edu>
2023-12-01 10:23:49 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Header from "./SRC/Components/Header/Header";
-import Home from "./SRC/Pages/Home";
 import "remixicon/fonts/remixicon.css";
-
 export default function Page() {
   return (
     <>
